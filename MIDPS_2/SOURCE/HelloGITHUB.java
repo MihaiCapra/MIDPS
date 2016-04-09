6af579035a5193bfdbbcb98222d76aa1609d3291
Mihai Capră <mihai.capra27@gmail.com>
2016-04-09 20:37:00 +0300

    some source files(Python,Java,C++)

---
+++ MIDPS_2/SOURCE/HelloGITHUB.java
@@ -0,0 +1,8 @@
+
+public class HelloGITHUB {
+
+    public static void main(String[] args) {
+        System.out.println("\nWelcome to GITHUB!");
+    }
+
+}
---
+++ MIDPS_2/SOURCE/hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+	std::cout<<"\nWelcome to GITHUB!"<<std::endl;
+	return 0;
+}
